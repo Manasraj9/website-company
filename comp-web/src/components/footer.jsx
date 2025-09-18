26fdc5e293617a8348368c254c9376a25d345a08
Manas Raj <manasraj1357@gmail.com>
2025-09-18 11:34:11 +0530

    Replace Twitter icon with X icon and update link in footer

--- comp-web/src/components/footer.jsx
+++ comp-web/src/components/footer.jsx
@@ -11,9 +11,9 @@ const LinkedinIcon = () => (
         <path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z" />
     </svg>
 );
-const TwitterIcon = () => (
+const XIcon = () => (
     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="currentColor" className="h-6 w-6">
-        <path d="M19.633 7.997c.013.175.013.349.013.523 0 5.325-4.053 11.461-11.46 11.461-2.282 0-4.402-.661-6.186-1.809.324.037.636.05.973.05a8.07 8.07 0 0 0 5.001-1.721 4.036 4.036 0 0 1-3.767-2.793c.249.037.499.062.762.062.361 0 .724-.05 1.061-.137a4.027 4.027 0 0 1-3.23-3.953v-.05c.537.299 1.16.486 1.82.511a4.022 4.022 0 0 1-1.796-3.354c0-.748.199-1.434.548-2.032a11.457 11.457 0 0 0 8.306 4.215c-.062-.3-.1-.611-.1-.923a4.026 4.026 0 0 1 4.028-4.028c1.16 0 2.207.486 2.943 1.272a7.957 7.957 0 0 0 2.556-.973 4.03 4.03 0 0 1-1.771 2.22c.823-.087 1.6-.312 2.317-.627a8.21 8.21 0 0 1-2.019 2.083z" />
+        <path d="M18.901 1.153h3.68l-8.04 9.19L24 22.846h-7.406l-5.8-7.584-6.638 7.584H.474l8.6-9.83L0 1.154h7.594l5.243 7.184L18.901 1.153Zm-1.65 19.57h2.61L7.129 2.652H4.28l13.001 18.07Z"/>
     </svg>
 );
 
@@ -70,7 +70,7 @@ export default function Footer() {
                         <div className="flex gap-5 mt-4 sm:mt-0">
                             <a href="https://facebook.com/jhainstrument" target="_blank" rel="noopener noreferrer" className="hover:text-white transition"><FacebookIcon /></a>
                             <a href="#" target="_blank" rel="noopener noreferrer" className="hover:text-white transition"><LinkedinIcon /></a>
-                            <a href="#" target="_blank" rel="noopener noreferrer" className="hover:text-white transition"><TwitterIcon /></a>
+                            <a href="https://x.com/JhaInstrument" target="_blank" rel="noopener noreferrer" className="hover:text-white transition"><XIcon /></a>
                         </div>
                     </div>
                 </div>
